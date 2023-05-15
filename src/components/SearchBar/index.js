9ee715b8ec3fc3915b3ef895103e2f6cf8304fe2
k-j-mc <kristofermcc@gmail.com>
2023-05-15 14:16:09 +0300

    search improvements

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import GithubContext from "../../context/github/githubContext";
 
@@ -10,9 +10,24 @@ const SearchBar = () => {
 	const githubContext = useContext(GithubContext);
 
 	const [searchQuery, setSearchQuery] = useState(githubContext.searchQuery);
+	const [isDisabled, setIsDisabled] = useState(true);
+
+	useEffect(() => {
+		if (searchQuery.replace(/\s/g, "").length) {
+			setIsDisabled(false);
+		} else {
+			setIsDisabled(true);
+		}
+	}, [searchQuery]);
 
 	const handleSearch = () => {
 		githubContext.searchUsers(searchQuery);
+	};
+
+	const enterSearch = (e) => {
+		if (e.keyCode === 13 && searchQuery.replace(/\s/g, "").length) {
+			handleSearch();
+		}
 	};
 
 	const clearSearch = () => {
@@ -31,6 +46,7 @@ const SearchBar = () => {
 					fullWidth
 					autoComplete="off"
 					onChange={(e) => setSearchQuery(e.target.value)}
+					onKeyDown={enterSearch}
 					InputProps={{
 						endAdornment: (
 							<>
@@ -46,6 +62,7 @@ const SearchBar = () => {
 			</Grid>
 			<Grid item xs={2} sm={1}>
 				<Button
+					disabled={isDisabled}
 					variant="contained"
 					color="primary"
 					onClick={handleSearch}
